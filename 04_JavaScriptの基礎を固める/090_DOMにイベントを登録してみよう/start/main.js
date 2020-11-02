cdd5da4cc3600798844bda09cc61cb872e8bc068
KodamaHiroaki <flere2100qp@gmail.com>
2020-11-03 00:58:16 +0900

    finishde_6-69

--- 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
@@ -1,10 +1,33 @@
 const btn = document.querySelector('#btn');
+const h1 = document.querySelector('h1');
+// btn.addEventListener('click', function () {
+//   alert('hello');
+// });
+// 無名関数での書き方
 
-const hello = function () {
+// const hello = function () {
+//   alert('hello');
+// };
+// btn.addEventListener('click', hello);
+// btn.addEventListener('mouseenter', hello);
+// btn.removeEventListener('click', hello);
+
+// 違う動作で同じ関数を使いまわしたい時にいちいち書かないで済むので、関数は外だしの方がベター
+// また登録したイベントを解除したい場合も、removeEventListenerの関数が必要になるので、外だしがベター
+
+function changecolor() {
   btn.style.color = 'red';
+  h1.style.color = 'blue';
 };
 
-btn.addEventListener('click', hello);
-btn.removeEventListener('click', hello);
+function changeBgcolor() {
+  h1.style.backgroundColor = 'green';
+};
 
-// 関数を外だししておいた方が、追加も削除も楽
+btn.addEventListener('click', changecolor);
+btn.addEventListener('click', changeBgcolor);
+
+// functionの中で定めたプロパティを関数の外に定義されているものにも適用できるので、例えば関数の中でh1のプロパティを定めたら、関数の外にあるh1に対してもプロパティを適用できる。
+
+// イベントハンドラーという1つしか関数を登録できない方法もあるが、基本使わない
+// htmlにイベントを定義し、関数を呼び出す方法もあるが、javascriptの記述がhtmlにあると管理できなくなるので、非推奨
--- 04_JavaScriptの基礎を固める/100_DOMイベント（ページの初期化）/start/main.js
+++ 04_JavaScriptの基礎を固める/100_DOMイベント（ページの初期化）/start/main.js
@@ -1,10 +1,14 @@
-const dcl = document.querySelector('.dcl');
-const load = document.querySelector('.load');
+// const dcl = document.querySelector('.dcl');
+// const load = document.querySelector('.load');
 
 document.addEventListener("DOMContentLoaded", function () {
+    const h1 = document.querySelector('h1');
+    h1.style.color = 'blue';
     dcl.classList.add('done');
 });
 
 window.addEventListener("load", function () {
+    const h1 = document.querySelector('h1');
+    h1.style.color = 'green';
     load.classList.add('done');
 });
